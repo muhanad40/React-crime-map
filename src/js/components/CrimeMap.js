6093f00d0641b091c1394df500df5ba2b1e3fb57
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2017-05-31 00:15:23 +0100

    Fetch crimes before map component mounts

--- src/js/components/CrimeMap.js
+++ src/js/components/CrimeMap.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchCrimes } from '../actions'
 
 export class CrimeMap extends Component {
+	componentWillMount() {
+		this.props.fetchCrimes()
+	}
+
     render() {
         return (
         	<div className="js-map">[ MAP ]</div>
@@ -8,4 +14,18 @@ export class CrimeMap extends Component {
     }
 }
 
-export default CrimeMap
+let mapStateToProps = (state) => {
+	return {
+		crimes: state.appReducer.crimes
+	}
+}
+
+let mapDispatchToProps = (dispatch) => {
+	return {
+        fetchCrimes: () => {
+            dispatch(fetchCrimes())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CrimeMap)
